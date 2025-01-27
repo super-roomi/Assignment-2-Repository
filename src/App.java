be404ba4e57b228ee65af36ba11a743dcc0551ec
Roomi <roomiisdead@gmail.com>
2025-01-27 19:30:17 +0300

    Basic implementation of Peer-to-Peer connectivity

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java Peer <port>");
+            System.exit(1);
+        }
+
+        int port = Integer.parseInt(args[0]);
+        Peer peer = new Peer(port);
+        peer.start();
     }
 }
